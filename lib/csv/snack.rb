07f60d6a27fc2437006d588d5b5a367da2e59496
andycooxy <andycooxy@gmail.com>
2019-12-30 07:35:27 +0000

    Init

---
+++ lib/csv/snack.rb
@@ -0,0 +1,8 @@
+require "csv/snack/version"
+
+module Csv
+  module Snack
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/csv/snack_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Csv::Snack do
+  it "has a version number" do
+    expect(Csv::Snack::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
